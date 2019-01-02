e889686770250ec9a72f39839a0ee5b3e17d4466
Nora <norapuskas@yahoo.com>
2019-01-02 18:14:38 +0100

    create react app that counts

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Header } from './Header';
+
+export const App = () => {
+  return (
+    <div>
+      <Header name='Norlandia'/>
+      <Header name='Norlandia'/>
+
+    </div>
+  );
+};
+
+// const App = () => {
+//   return ( 
+//     <div>
+//       <Header />
+//     </div>
+//    );
+// }
+ 
+// export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.name,
+      count: 0,
+    };
+  }
+
+  increase() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          backgroundColor: `rgb(
+            ${this.state.count},
+            ${this.state.count},
+            ${this.state.count}
+          )`,
+        }}
+      >
+        <h1>{`${this.state.name} : ${this.state.count}`}</h1>
+        <button onClick={() => this.increase()}>Increase</button>
+      </div>
+    );
+  }
+}
+
+// import React, { Component } from 'react';
+
+// class Header extends Component {
+//   state = {
+//     name: 'Norlandia',
+//     count: 0,
+//   };
+
+//   increase() {
+//     this.setState({
+//       count: this.state.count++,
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>{`${this.state.name} : ${this.state.count}`}</h1>
+//         <button onClick={() => this.increase()}>Increase</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Header;
